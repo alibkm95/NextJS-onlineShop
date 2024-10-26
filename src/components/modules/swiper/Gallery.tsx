a67c93e6cac22376c71c2fa35d0cde3b59eb29c4
alibkm95 <ali.bkm95@gmail.com>
2024-10-26 11:45:36 +0330

    fix(ui): gallery thumbs ui and styles fix

--- src/components/modules/swiper/Gallery.tsx
+++ src/components/modules/swiper/Gallery.tsx
@@ -3,13 +3,14 @@ import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Navigation, Thumbs, Zoom } from "swiper/modules";
 import SwiperCore from "swiper";
+import Image from "next/image";
 
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 import "swiper/css/zoom";
-import Image from "next/image";
+import "@/styles/Gallery.modules.css";
 
 const fakeGallery = [
   "https://placehold.co/640x360/orange/white/png",
@@ -21,50 +22,51 @@ const fakeGallery = [
 const Gallery = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperCore | null>(null);
   return (
-    <div className="p-2 rounded-lg bg-secondary">
-      <Swiper
-        zoom={true}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[Zoom, Navigation, Thumbs]}
-        className="swiperGallery aspect-video"
-      >
-        {fakeGallery.map((img, index) => (
-          <SwiperSlide key={index}>
-            <div className="swiper-zoom-container">
+    <div className="rounded-lg bg-secondary max-w-full p-2">
+      <div>
+        <Swiper
+          zoom={true}
+          spaceBetween={10}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[Zoom, Thumbs]}
+          className="swiperGallery cursor-move"
+        >
+          {fakeGallery.map((img, index) => (
+            <SwiperSlide key={index}>
+              <div className="swiper-zoom-container">
+                <Image
+                  src={img}
+                  className="rounded-md w-full"
+                  width={640}
+                  height={360}
+                  alt="product gallery image"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <Swiper
+          onSwiper={(swiper: SwiperCore) => setThumbsSwiper(swiper)}
+          spaceBetween={10}
+          slidesPerView={3}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="swiperGalleryThumb mt-4 cursor-pointer"
+        >
+          {fakeGallery.map((img, index) => (
+            <SwiperSlide key={index}>
               <Image
                 src={img}
-                className="w-full rounded-md"
                 width={640}
                 height={360}
-                alt="product gallery image"
+                className="rounded-md"
+                alt="product gallery thumb"
               />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <Swiper
-        onSwiper={(swiper: SwiperCore) => setThumbsSwiper(swiper)}
-        spaceBetween={10}
-        slidesPerView={3}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="swiperGalleryThumb mt-4"
-      >
-        {fakeGallery.map((img, index) => (
-          <SwiperSlide key={index}>
-            <Image
-              src={img}
-              width={640}
-              height={360}
-              className="rounded-md"
-              alt="product gallery thumb"
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
--- src/components/templates/product/ProductDetails.tsx
+++ src/components/templates/product/ProductDetails.tsx
@@ -1,12 +1,15 @@
-import Gallery from '@/components/modules/swiper/Gallery'
-import React from 'react'
+import Gallery from "@/components/modules/swiper/Gallery";
+import React from "react";
 
 const ProductDetails = () => {
   return (
-    <div className="grid gap-4 lg:grid-cols-2">
-      <Gallery />
+    <div className="min-h-96">
+      <div className="grid gap-4 grid-cols-1 lg:grid-cols-2 max-w-full">
+        <Gallery />
+        <div className=""></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
